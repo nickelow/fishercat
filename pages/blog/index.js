655b48256f3ae6e63a7663f6d5bd7c263570e955
nickelow <matthewpnicolaou@gmail.com>
2025-01-16 14:12:00 -0700

    adding bloghandy

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,88 +1,27 @@
 import React from "react";
+import Head from "next/head";
 
 export default function Blog() {
+  React.useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://www.bloghandy.com/api/bh_blogengine.js";
+    script.async = true;
+    script.onload = () => this.scriptLoaded();
+    document.body.appendChild(script);
+  }, []);
+
   return (
-    <section class="text-white-600 text-base body-font bg-black">
-      <div class="max-w-5xl pt-52 pb-24 mx-auto flex flex-col items-center content">
-        <img
-          class="max-w-full h-auto p-4"
-          src="/images/fishercat.png"
-          height="300"
-          width="300"
-        ></img>
-        <p class="indent">
-          Throughout my career, I’ve been fortunate to work with a lot of
-          incredible founders, leaders, and fellow recruiters. I’m grateful that
-          I’ve had the opportunity to learn from such talented people, seeing
-          firsthand how some of the best organizations have hired.
-        </p>
-        <br />
-        <p class="indent">
-          Building and scaling a team, especially at an early stage startup, is
-          unique. What works for some organizations may not work for others, and
-          there’s a delicate balance between establishing enough processes to be
-          efficient without overcomplicating things.
-        </p>
-        <br />
-        <p class="indent">
-          I’ve been able to experience recruiting and teambuilding from
-          different angles, which has given me a unique perspective on the
-          process. From my time as a recruiter at an executive search agency, as
-          an in-house recruiter supporting venture-backed startups, and also
-          from the perspective of a venture capital firm where I helped
-          portfolio companies hire and scale, one thing I’ve realized is that
-          there’s no shortage of aspiring founders with great ideas, but the
-          ability to assemble a high-performing team is a significant challenge.
-        </p>
-        <br />
-        <p class="indent">
-          Recruiting and interviewing can be time-consuming and expensive. After
-          hiring and placing hundreds of professionals and helping founders
-          establish practical recruiting strategies, I realized that a scalable
-          approach to recruiting and team-building was sorely needed in the
-          industry.
-        </p>
-        <br />
-        <p class="indent">
-          There is a unique opportunity to think through recruiting more
-          holistically than ever before, simultaneously tackling both the
-          recruiting strategy as well as executing on the tactical aspects of
-          hiring. The best agency recruiters in the world can help companies
-          hire great people, but it can be costly and may not be scalable. For
-          many startups, it may not always be the right time to hire full-time
-          recruiting support, and in-house recruiters are often stretched thin
-          and may struggle to adjust to the high-paced demands of a startup.
-        </p>
-        <br />
-        <p class="indent">
-          I’ve decided to found Fishercat Consulting to partner with emerging
-          high-growth companies to establish a scalable approach to recruiting.
-          Over the last year and a half, I’ve been lucky enough to support early
-          stage organizations (typically seed to Series C) with their hiring and
-          recruiting across their sales, engineering, product, and design teams,
-          and I’m tremendously excited to publicly launch Fishercat Consulting.
-        </p>
-        <br />
-        <p class="indent">
-          From the time I first encountered a fishercat on a hike in the woods
-          near my childhood home, I’ve been fascinated by the animal. Despite
-          their relatively small size (typically less than twenty pounds),
-          they’ve been able to carve out their own place in the food chain,
-          competing with larger predators like bears, bobcats, and mountain
-          lions and hunting game like deer and porcupines. It’s a tenacious
-          animal, and I thought that the name, “Fishercat Consulting,” reflected
-          not only my New England roots, but also the classic startup dynamic of
-          a lean, hungry up-and-comer competing with larger and more established
-          companies.
-        </p>
-        <br />
-        <p class="indent">
-          Fishercat Consulting is a flexible, efficient, and cost-effective
-          alternative to traditional recruiting agencies and in-house talent
-          teams. If you think Fishercat Consulting is the recruiting partner
-          you’re after, don’t hesitate to reach out!
-        </p>
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Fishercat Consulting</title>
+        <link rel="icon" href="/favicon.png" />
+        <script>var bh_id = "60CUPq6D3w810VNTA3MQ";</script>
+      </Head>
+      <section class="text-white-600 text-base body-font bg-black">
+        <div class="max-w-5xl pt-52 pb-24 mx-auto flex flex-col items-center content">
+          <div id="bh-posts"></div>
+        </div>
+      </section>
+    </>
   );
 }
